da39b7ba3a77990f37d5c2d15eef8698b24b78ab
Spigell <spigelly@gmail.com>
2025-11-12 18:39:25 +0700

    :wrench: reinvert gateway project

--- output-gateway/index.ts
+++ output-gateway/index.ts
@@ -1,32 +1,30 @@
 import * as pulumi from '@pulumi/pulumi';
 
-type NamespacedOutputs = Record<string, unknown>;
+type NamespacedOutputs = Record<string, pulumi.Input<unknown>>;
 type OutputsToReexport = Record<string, NamespacedOutputs>;
 
 const sourceStack = 'organization/my-cloud-identity/main';
 const stackRef = new pulumi.StackReference(sourceStack);
 const currentStack = pulumi.getStack();
 
-const outputsToReexport = stackRef.requireOutput(
+const outputsToReexport = stackRef.getOutput(
   'outputsToReexport'
 ) as pulumi.Output<OutputsToReexport>;
 
-outputsToReexport.apply((namespaces) => {
-  const namespacedOutputs = namespaces?.[currentStack];
-
-  if (!namespaces || !namespacedOutputs) {
-    throw new pulumi.RunError(
-      `No outputsToReexport entry found for stack "${currentStack}".`
-    );
-  }
-
-  const exportOutput = (
-    pulumi.runtime as unknown as {
-      export(name: string, value: pulumi.Input<unknown>): void;
+export const output = outputsToReexport.apply(
+  (namespaces: OutputsToReexport | undefined) => {
+    if (!namespaces) {
+      throw new pulumi.RunError('No outputsToReexport definition found.');
     }
-  ).export;
 
-  Object.entries(namespacedOutputs).forEach(([outputName, value]) => {
-    exportOutput(outputName, value as pulumi.Input<unknown>);
-  });
-});
+    const namespacedOutputs = namespaces[currentStack];
+
+    if (!namespacedOutputs) {
+      throw new pulumi.RunError(
+        `No outputsToReexport entry found for stack "${currentStack}".`
+      );
+    }
+
+    return namespacedOutputs;
+  }
+);
